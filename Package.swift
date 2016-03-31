aa02ee04d06bc2d9432e077b0674a265f178f9f8
Scott Byrns <contact@scottbyrns.com>
2016-03-31 00:51:18 -0600

    Blob support

--- Package.swift
+++ Package.swift
@@ -3,7 +3,9 @@ import PackageDescription
 let package = Package(
     name: "CrateIO",
     dependencies: [
-        .Package(url: "https://github.com/scottbyrns/Socks.git", majorVersion: 0, minor: 0),
-        .Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 4)
+        .Package(url: "https://github.com/czechboy0/Socks.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/Zewo/Digest.git", majorVersion: 0, minor: 0),
+        .Package(url: "https://github.com/Zewo/Data.git", majorVersion: 0, minor: 4)
     ]
 )
--- Sources/CrateIO.swift
+++ Sources/CrateIO.swift
@@ -2,13 +2,18 @@
 import SocksCore
 import JSON
 import Foundation
+import Data
+import Digest
 
 public class CrateIO {
 
     let socket : InternetSocket
+    let host: String
+    let port: UInt16
 
   	public init(withHost host: String, onPort port: UInt16) throws {
-
+      self.host = host
+      self.port = port
       let raw = try! RawSocket(protocolFamily: .Inet, socketType: .Stream, protocol: .TCP)
       let addr = InternetAddress(address: .Hostname(host), port: port)
       socket = InternetSocket(rawSocket: raw, address: addr)
@@ -16,7 +21,7 @@ public class CrateIO {
 
   	}
 
-    public func sql (statement: String) -> JSON {
+    public func sql (statement: String) -> JSON? {
       print("\(statement)")
 
       let post = "{\"stmt\": \"\(statement)\"}\r\n"
@@ -37,8 +42,87 @@ public class CrateIO {
       //yay!
       print("Received: \n\(str)")
       // let NSUTF8StringEncoding = 8
-      return try! JSONParser().parse(Data(NSString(string: str).componentsSeparatedByString("\r\n\r\n")[1]))
+      do {
+        return try  JSONParser().parse(Data(NSString(string: str).componentsSeparatedByString("\r\n\r\n")[1]))
+      }
+      catch {
+        return nil
+      }
     }
+
+
+
+
+    public func blob (insert data: String, into table: String) -> String? {
+
+      let digest = Digest.sha1(data)
+
+      // let post = "{\"stmt\": \"\(statement)\"}\r\n"
+      let postBytes = data.toBytes()
+
+      let request = "PUT /_blobs/\(table)/\(digest) HTTP/1.1\r\nHost: \(self.host):\(self.port)\r\nUser-Agent: curl/7.43.0\r\nAccept: */*\r\nContent-Length: \(postBytes.count)\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\n"
+
+      print(request)
+      try! socket.send(request.toBytes())
+      try! socket.send(data.toBytes())
+
+      //receiving data
+      let received = try! socket.recv()
+
+      //converting data to a string
+      let str = try! received.toString()
+
+      //yay!
+      print("Received: \n\(str)")
+      // let NSUTF8StringEncoding = 8
+
+
+      if NSString(string: str).componentsSeparatedByString("201 Created").count > 1 {
+        return digest
+      }
+
+      return nil
+    }
+
+
+
+    public func blob (fetch digest: String, from table: String) -> String? {
+
+
+      let request = "GET /_blobs/\(table)/\(digest) HTTP/1.1\r\n\r\n"
+
+//      print(request)
+      try! socket.send(request.toBytes())
+
+
+      //receiving data
+      let received = try! socket.recv()
+
+      //converting data to a string
+      let str = try! received.toString()
+
+      print(str)
+      if NSString(string: str).componentsSeparatedByString("200").count > 1 {
+
+      }
+      else {
+        return nil
+      }
+      //
+      // //receiving data
+      // let receivedBlob = try! socket.recv()
+      //
+      // //converting data to a string
+      // let blob = try! receivedBlob.toString()
+      //
+      //
+
+      //yay!
+      // print("Received: \n\(blob)")
+      return NSString(string: str).componentsSeparatedByString("\r\n\r\n")[1]
+
+    }
+
 
     deinit {
         try! socket.close()
