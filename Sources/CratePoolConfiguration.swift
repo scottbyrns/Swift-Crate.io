22240fcc5d9440ba7f814913d645847f977b7a4a
Scott Byrns <contact@scottbyrns.com>
2016-04-02 22:19:38 -0600

    Correcting access level of pool config

--- Sources/CratePoolConfiguration.swift
+++ Sources/CratePoolConfiguration.swift
@@ -1,15 +1,15 @@
-class CratePoolConfiguration : PoolConfiguration {
+public class CratePoolConfiguration : PoolConfiguration {
 
     // Set how long a pool can suffer a continued series of errors before it is removed from the pool.
-    var maxErrorDuration: Duration = 1.minute
+    public var maxErrorDuration: Duration = 1.minute
 
     // Set how long to wait before trying to issue a connection to a consumer after finding none available.
-    var retryDelay: Duration = 10.milliseconds
+    public var retryDelay: Duration = 10.milliseconds
 
     // How long to wait for a connection to be available before giving up.
-    var connectionWait: Duration = 30.milliseconds
+    public var connectionWait: Duration = 30.milliseconds
 
     // How long to keep trying to reconnect a closed socket before removing it from the pool.
-    var maxReconnectDuration: Duration = 5.minutes
+    public var maxReconnectDuration: Duration = 5.minutes
 
 }
