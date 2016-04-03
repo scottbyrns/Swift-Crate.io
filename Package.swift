1549e124fab286760f01fd517dbaa7fa878c5472
Scott Byrns <contact@scottbyrns.com>
2016-04-02 22:12:48 -0600

    Implementing ConnectionPool for TCPClientSocket's

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/Zewo/Data.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/Zewo/HTTPParser.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/Zewo/HTTPSerializer.git", majorVersion: 0, minor: 4)
-
+        .Package(url: "https://github.com/Zewo/HTTPSerializer.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/scottbyrns/ConnectionPool.git", majorVersion: 0, minor: 0)
     ]
 )
--- Sources/CrateIO.swift
+++ Sources/CrateIO.swift
@@ -10,20 +10,13 @@ import S4
 
 public class CrateIO {
 
-    var sockets : [TCPClientSocket]
+    var socketPool : ConnectionPool<TCPClientSocket>
     var currentSocket = -1
     let hosts: [String]
     let ports: [Int]
 
-    public init(withHosts hosts: [String], onPorts ports: [Int]) throws {
-      self.hosts = hosts
-      self.ports = ports
-      sockets = [TCPClientSocket]()
-      for index in 0...hosts.count - 1 {
-        sockets.append(
-          try TCPClientSocket(ip: IP(remoteAddress: hosts[index], port: ports[index]))
-        )
-      }
+    public init(pool connections: [TCPClientSocket], using configuration: CratePoolConfiguration) throws {
+		socketPool = ConnectionPool<TCPClientSocket>(pool: connections, using: configuration)
     }
 
     private func nextSocket() -> TCPClientSocket {
@@ -36,84 +29,89 @@ public class CrateIO {
 
     public func sql (statement: String) throws -> JSON? {
 
-      let socket = nextSocket()
+        // Get a connection from the pool to use.
+	    return try pool.with({ connection in
+			
+	        let post = "{\"stmt\": \"\(statement)\"}\r\n"
+	        let postBytes = [UInt8](post.utf8)
 
-      let post = "{\"stmt\": \"\(statement)\"}\r\n"
-      let postBytes = [UInt8](post.utf8)
+	        let headers: Headers = [
+	            "Content-Length": HeaderValues("\(postBytes.count)"),
+	            "Content-Type": HeaderValues("application/json"),
+	            "User-Agent": HeaderValues("Swift-CrateIO")
+	        ]
 
-      let headers: Headers = [
-          "Content-Length": HeaderValues("\(postBytes.count)"),
-          "Content-Type": HeaderValues("application/json"),
-          "User-Agent": HeaderValues("Swift-CrateIO")
-      ]
+	        let request = try Request(method: .post, uri: "/_sql", headers: headers, body: post)
 
-      let request = try Request(method: .post, uri: "/_sql", headers: headers, body: post)
+	        let requestData = Data(try requestToString(request))
+	        try! connection.send(requestData)
 
-      let requestData = Data(try requestToString(request))
-      try! socket.send(requestData)
+	        //receiving data
+	        let received = try connection.receive(lowWaterMark: 1, highWaterMark: 1024*1024)
 
-      //receiving data
-      let received = try socket.receive(lowWaterMark: 1, highWaterMark: 1024*1024)
+	        //converting data to a string
+	        let str = try String(data: received)
 
-      //converting data to a string
-      let str = try String(data: received)
+	        // print("received: \(str)")
 
-      // print("received: \(str)")
+	        let parser = ResponseParser()
 
-      let parser = ResponseParser()
+	        do {
+	          if let response = try parser.parse(str) {
+	            switch response.body {
+	              case .buffer(let data):
+	                return try JSONParser().parse(data)
+	              default:
+	                break
+	            }
+	          }
+	        }
+	        catch {
 
-      do {
-        if let response = try parser.parse(str) {
-          switch response.body {
-            case .buffer(let data):
-              return try JSONParser().parse(data)
-            default:
-              break
-          }
-        }
-      }
-      catch {
-
-      }
-      return nil
+	        }
+			
+	    })
 
     }
 
 
     public func blob (insert data: Data, into table: String) throws -> String? {
 
-      let socket = nextSocket()
+	    // Get a connection from the pool to use.
+	    return try pool.with({ connection in
 
-      let digest = Digest.sha1(try String(data: data))
+	        let digest = Digest.sha1(try String(data: data))
 
-      let headers: Headers = [
-          "Content-Length": HeaderValues("\(data.bytes.count)"),
-          "Content-Type": HeaderValues("application/x-www-form-urlencoded"),
-          "User-Agent": HeaderValues("Swift-CrateIO")
-      ]
+	        let headers: Headers = [
+	            "Content-Length": HeaderValues("\(data.bytes.count)"),
+	            "Content-Type": HeaderValues("application/x-www-form-urlencoded"),
+	            "User-Agent": HeaderValues("Swift-CrateIO")
+	        ]
 
-      let request = try Request(method: .put, uri: "/_blobs/\(table)/\(digest)", headers: headers, body: data)
+	        let request = try Request(method: .put, uri: "/_blobs/\(table)/\(digest)", headers: headers, body: data)
 
-      try! socket.send(Data(requestToString(request)))
+	        try! connection.send(Data(requestToString(request)))
 
-      //receiving data
-      let received = try socket.receive(lowWaterMark: 1, highWaterMark: 1024)
-      //converting data to a string
-      let str = try String(data: received)
+	        //receiving data
+	        let received = try connection.receive(lowWaterMark: 1, highWaterMark: 1024)
+	        //converting data to a string
+	        let str = try String(data: received)
 
-      let parser = ResponseParser()
+	        let parser = ResponseParser()
 
-      do {
-        if let response = try parser.parse(str) {
-          if response.statusCode == 201 {
-            return digest
-          }
-        }
-      }
-      catch {
+	        do {
+	          if let response = try parser.parse(str) {
+	            if response.statusCode == 201 {
+	              return digest
+	            }
+	          }
+	        }
+	        catch {
 
-      }
-      return nil
+	        }
+			
+	    })
+		
     }
 
 
@@ -122,43 +120,44 @@ public class CrateIO {
 
     public func blob (fetch digest: String, from table: String) throws -> Data {
 
-      let socket = nextSocket()
+        // Get a connection from the pool to use.
+        return try pool.with({ connection in
 
-      let headers: Headers = [
-          "User-Agent": HeaderValues("Swift-CrateIO")
-      ]
+	        let headers: Headers = [
+	            "User-Agent": HeaderValues("Swift-CrateIO")
+	        ]
 
-      let request = try Request(method: .get, uri: "/_blobs/\(table)/\(digest)", headers: headers)
-
-
-      try socket.send(Data(requestToString(request)))
+	        let request = try Request(method: .get, uri: "/_blobs/\(table)/\(digest)", headers: headers)
 
 
-      //receiving data
-      let received = try socket.receive(lowWaterMark: 1, highWaterMark: 1024)
-      //converting data to a string
-      let str = try String(data: received)
+	        try connection.send(Data(requestToString(request)))
 
 
-      let parser = ResponseParser()
+	        //receiving data
+	        let received = try connection.receive(lowWaterMark: 1, highWaterMark: 1024)
+	        //converting data to a string
+	        let str = try String(data: received)
 
-      do {
-        if let response = try parser.parse(str) {
-          if response.statusCode == 200 {
-            switch response.body {
-              case .buffer(let data):
-                return data
-              default:
-                break
-            }
-          }
-        }
-      }
-      catch {
 
-      }
-      return nil
+	        let parser = ResponseParser()
 
+	        do {
+	          if let response = try parser.parse(str) {
+	            if response.statusCode == 200 {
+	              switch response.body {
+	                case .buffer(let data):
+	                  return data
+	                default:
+	                  break
+	              }
+	            }
+	          }
+	        }
+	        catch {
+
+	        }
+			
+        })
 
     }
 
